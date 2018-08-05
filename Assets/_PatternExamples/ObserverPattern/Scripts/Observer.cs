00902c1850eb38f120b2d241a7486cff5d1e71dc
DESKTOP-QQLJCR4\Fatshih <alan0201tw@gmail.com>
2018-08-05 14:04:36 +0800

    add observer pattern

---
+++ Assets/_PatternExamples/ObserverPattern/Scripts/Observer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatternExample.ObserverPattern
+{
+    public class Observer : MonoBehaviour
+    {
+        [SerializeField]
+        private Subject observingSubject;
+
+        private void Start()
+        {
+            observingSubject.OnBigImpact += () =>
+            {
+                Debug.Log("Observered BigImpact!");
+            };
+        }
+    }
+}
---
+++ Assets/_PatternExamples/ObserverPattern/Scripts/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace DesignPatternExample.ObserverPattern
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
+    public class Subject : MonoBehaviour
+    {
+        public event Action OnBigImpact;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.impulse.magnitude > 5f)
+            {
+                if(OnBigImpact != null)
+                {
+                    OnBigImpact.Invoke();
+                }
+            }
+        }
+    }
+}
