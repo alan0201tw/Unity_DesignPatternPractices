d2ead52e180e9d65b4f012bd2803883b9cad2481
DESKTOP-QQLJCR4\Fatshih <alan0201tw@gmail.com>
2018-08-25 00:57:11 +0800

    add object pool pattern

---
+++ Assets/_PatternExamples/ObjectPoolPattern/Scripts/Demo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatternExample.ObjectPoolPattern
+{
+    public class Demo : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioClip m_clip;
+
+        private ObjectPool<AudioSource> m_audioSourcePool;
+
+        private void Awake()
+        {
+            m_audioSourcePool = new ObjectPool<AudioSource>(() =>
+            {
+                GameObject dummyGameObject = new GameObject("Dummy Audio Source");
+                dummyGameObject.transform.parent = transform;
+                dummyGameObject.SetActive(false);
+                return dummyGameObject.AddComponent<AudioSource>();
+            }, 5, true);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                AudioSource source = m_audioSourcePool.GetObject();
+                source.gameObject.SetActive(true);
+                source.PlayOneShot(m_clip);
+            }
+
+            foreach(var source in m_audioSourcePool.UsedObjects)
+            {
+                if (source.isPlaying == false)
+                {
+                    m_audioSourcePool.ReleaseObject(source);
+                    source.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_PatternExamples/ObjectPoolPattern/Scripts/ObjectPool.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DesignPatternExample.ObjectPoolPattern
+{
+    public class ObjectPool<T> where T : class
+    {
+        private List<ObjectPoolUnit> m_pool = new List<ObjectPoolUnit>();
+        private Dictionary<T, ObjectPoolUnit> m_objectBeingUsed = new Dictionary<T, ObjectPoolUnit>();
+
+        private Func<T> m_factoryFunction;
+        private bool m_isExtendable;
+
+        public int PoolSize
+        {
+            get { return m_pool.Count; }
+        }
+
+        public int UsedObjectCount
+        {
+            get { return m_objectBeingUsed.Count; }
+        }
+
+        public List<T> UsedObjects
+        {
+            get  { return m_objectBeingUsed.Keys.ToList(); }
+        }
+
+        public ObjectPool(Func<T> factoryFunction, int poolStartingSize, bool isExtendable)
+        {
+            // do validation
+            if (factoryFunction == null)
+                throw new Exception("ObjectPool constructor : factoryFunction cannot be null.");
+            if (poolStartingSize <= 0)
+                throw new Exception("ObjectPool constructor : poolStartingSize must be greater than 0.");
+
+            // initialize
+            m_isExtendable = isExtendable;
+            m_factoryFunction = factoryFunction;
+
+            for (int i = 0; i < poolStartingSize; i++)
+            {
+                CreateNewUnit();
+            }
+        }
+
+        private ObjectPoolUnit CreateNewUnit()
+        {
+            // create new units with passed-in factory method
+            ObjectPoolUnit newUnit = new ObjectPoolUnit(m_factoryFunction.Invoke());
+            m_pool.Add(newUnit);
+
+            return newUnit;
+        }
+
+        public T GetObject()
+        {
+            ObjectPoolUnit freeUnit = null;
+            for (int i = 0; i < m_pool.Count; i++)
+            {
+                // if this unit is being used, continue to find a free one
+                if (m_pool[i].IsBeingUsed == true)
+                    continue;
+
+                // if a free unit is found, store it and break the loop
+                freeUnit = m_pool[i];
+                break;
+            }
+
+            if (freeUnit == null)
+            {
+                if (m_isExtendable == false)
+                {
+                    // since we constraint T as class, this will always be null
+                    // if T can be data types or structs, the default value can vary
+                    return default(T);
+                }
+                else
+                {
+                    freeUnit = CreateNewUnit();
+                }
+            }
+
+            freeUnit.IsBeingUsed = true;
+            m_objectBeingUsed.Add(freeUnit.ObjectInstance, freeUnit);
+            return freeUnit.ObjectInstance;
+        }
+
+        public void ReleaseObject(T releasingObject)
+        {
+            if (m_objectBeingUsed.ContainsKey(releasingObject))
+            {
+                ObjectPoolUnit releasingUnit = m_objectBeingUsed[releasingObject];
+                releasingUnit.IsBeingUsed = false;
+                m_objectBeingUsed.Remove(releasingObject);
+            }
+            else
+            {
+                Debug.LogWarning("Object Pool of Type : " + typeof(T) + " trying to release object : " + releasingObject + ", which is not in pool.");
+            }
+        }
+
+        private class ObjectPoolUnit
+        {
+            private T m_instance;
+            public T ObjectInstance
+            {
+                get { return m_instance; }
+            }
+
+            private bool m_isBeingUsed;
+            public bool IsBeingUsed
+            {
+                get { return m_isBeingUsed; }
+                set { m_isBeingUsed = value; }
+            }
+
+            public ObjectPoolUnit(T instance)
+            {
+                m_instance = instance;
+                m_isBeingUsed = false;
+            }
+        }
+    }
+
+}
