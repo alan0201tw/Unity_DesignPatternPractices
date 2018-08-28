69034626364a8f9c51c911f436aef477b779f64d
DESKTOP-QQLJCR4\Fatshih <alan0201tw@gmail.com>
2018-08-28 16:13:35 +0800

    Add state pattern

---
+++ Assets/_PatternExamples/StatePattern/Scripts/Demo.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatternExample.StatePattern
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class Demo : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider m_collider;
+        [SerializeField]
+        private Rigidbody m_rigidbody;
+
+        private void Reset()
+        {
+            m_collider = GetComponent<Collider>();
+            m_rigidbody = GetComponent<Rigidbody>();
+        }
+
+        [SerializeField]
+        private State m_state = State.Idle;
+
+        private void Update()
+        {
+            switch (m_state)
+            {
+                case State.Idle:
+                case State.Walking:
+
+                    State nextState = State.Idle;
+
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        Vector3 newVel = new Vector3(-1, m_rigidbody.velocity.y, 0);
+                        m_rigidbody.velocity = newVel;
+                        nextState = State.Walking;
+                    }
+                    else if (Input.GetKey(KeyCode.D))
+                    {
+                        Vector3 newVel = new Vector3(1, m_rigidbody.velocity.y, 0);
+                        m_rigidbody.velocity = newVel;
+                        nextState = State.Walking;
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.W))
+                    {
+                        m_rigidbody.AddForce(Vector3.up * 400f);
+                        nextState = State.Jumping;
+                    }
+
+                    m_state = nextState;
+
+                    break;
+
+                case State.Jumping:
+                    if (Input.GetKey(KeyCode.A))
+                    {
+                        Vector3 newVel = new Vector3(-1, m_rigidbody.velocity.y, 0);
+                        m_rigidbody.velocity = newVel;
+                    }
+                    else if (Input.GetKey(KeyCode.D))
+                    {
+                        Vector3 newVel = new Vector3(1, m_rigidbody.velocity.y, 0);
+                        m_rigidbody.velocity = newVel;
+                    }
+
+                    // I mean... you should probably do some ground check in your actual game,
+                    // but I'll just do this here since it's just a demo.
+                    // Please don't judge me for this :(
+                    if (transform.position.y <= 0.5f)
+                    {
+                        if (m_rigidbody.velocity.magnitude > 0)
+                            m_state = State.Walking;
+                        else
+                            m_state = State.Idle;
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/_PatternExamples/StatePattern/Scripts/State.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPatternExample.StatePattern
+{
+    enum State : byte
+    {
+        Idle,
+        Walking,
+        Jumping,
+        Ducking
+    }
+
+    // This part is sort of like the Strategy pattern, but it has the advantage for main object
+    // (in this case, the Demo component) to change its behavior by changing the stateCommand object.
+    // Please notice that the actual transitions between commands
+    // While in Strategy pattern
+
+    // you can also use this commmand instance to handle input
+    public abstract class StateCommand
+    {
+        // use protected since all commmand will be inherit from here, so exposing to them is enough
+        protected static IdleCommand idle = new IdleCommand();
+        protected static WalkingCommand walkingCommand = new WalkingCommand();
+
+        // this will return the next command, if it's null, the state remains unchanged
+        public abstract StateCommand HangleInput(Demo demo);
+
+        // in some case these might come in handy
+        // you can call these in the main object (in this case, Demo component) when a transition
+        // between stateCommands occurs.
+
+        // public abstract void OnEnterState();
+        // public abstract void OnExitState();
+    }
+
+    public class IdleCommand : StateCommand
+    {
+        public override StateCommand HangleInput(Demo demo)
+        {
+            StateCommand stateCommand = null;
+            var m_rigidbody = demo.GetComponent<Rigidbody>();
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                Vector3 newVel = new Vector3(-1, m_rigidbody.velocity.y, 0);
+                m_rigidbody.velocity = newVel;
+                stateCommand = walkingCommand;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                Vector3 newVel = new Vector3(1, m_rigidbody.velocity.y, 0);
+                m_rigidbody.velocity = newVel;
+                stateCommand = walkingCommand;
+            }
+
+            return stateCommand;
+        }
+    }
+
+    public class WalkingCommand : StateCommand
+    {
+        public override StateCommand HangleInput(Demo demo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
